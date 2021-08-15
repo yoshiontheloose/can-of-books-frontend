c48e39ddae9d2a4b9c886bd5436481c05b109720
Clarissa P <clarissapamonicutt@gmail.com>
2021-08-14 21:59:39 -0700

    most of lab11 donoe

--- src/Login.js
+++ src/Login.js
@@ -2,17 +2,18 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import './Login.css';
+import LoginButton from './LoginButton.js';
 
 class Login extends React.Component {
   render() {
-    return(
+    return (
       <Card style={{ width: '18rem' }}>
         <Card.Body>
           <Card.Title>Log In</Card.Title>
           <Card.Text>
             Click Below to Log In
           </Card.Text>
-          {/* TODO: add a `LoginButton` component here that will log the user in with Auth0 */}
+          <LoginButton />
         </Card.Body>
       </Card>
     )
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 // TODO: wrap everything in Auth0
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-wajr50en.us.auth0.com"
+      clientId="OicWcl01VU13GLT7FXyt0VbPSxGlNbSd"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+//NOTE: Do we have to put the domain and clientId into the .env file? There's an alert on log in page about Dev keys
